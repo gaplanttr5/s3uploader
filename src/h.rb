c20a9b6226f7af1d9574100848d5287992e069b9
mitsuru sakamoto <msakamotoster@gmail.com>
2019-05-19 13:41:12 +0900

    ベタ書きやめよう。

--- src/h.rb
+++ src/h.rb
@@ -1,7 +1,10 @@
 pp 'HelloWorld'
 require 'aws-sdk-s3'
 
+local_path = './data/a/2020/03/10/a.json'
+s3_path = local_path.match(/.\/data\/(.*)$/)[1]
+
 s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
-obj = s3.bucket('msakamoto.athena-test').object('a/2019/03/10/a.json')
-obj.upload_file('./data/2019/03/10/a.json')
+obj = s3.bucket('msakamoto.athena-test').object(s3_path)
+obj.upload_file(local_path)
 
